d71cc8e39ca7ed52e789edb44bc62c7643ffd569
dsvivek007 <dsvivek007@gmail.com>
2018-02-19 17:09:24 -0500

    Added fascicles data to parameters

--- parameters.py
+++ parameters.py
@@ -17,3 +17,20 @@ class Parameters():
         self.Length = 0.3
         # Standard deviation of branch length
         self.Std_Length = np.sqrt(0.2)*self.Length
+        # Minmum length: prevents negative lengths
+        self.Min_Length = self.length/10
+        self.Branch_Angle = 45.0
+        self.w = 0.1
+        # Approximate length of each segment
+        self.S_Length = 0.01
+
+
+        self.Fascicles=True
+        ###########################################
+        # Fascicles data
+        ###########################################
+        self.fascicles_angles=[-1.5,.2] #rad
+        self.fascicles_length=[.5,.5]
+        # Save data?
+        self.save=True
+        self.save_paraview=True
